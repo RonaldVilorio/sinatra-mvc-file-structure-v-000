653d5c34fc9c17d47e62606fe5fb73cd3b6be58f
ronald vilorio <soccer23415@gmail.com>
2018-01-15 20:46:15 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,7 +2,7 @@ class Dog
   @@all = []
   attr_accessor :name,:breed,:id,:age
 
-  def initialize(name=nil,breed=nil,id=nil,age=nil)
+  def initialize(name=nil,breed=nil,id=nil)
     @name = name
     @breed = breed
     @id = id
